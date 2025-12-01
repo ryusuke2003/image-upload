ebf5897eba09b3ce259ee51aa500297a408b9d4c
Ryusuke2003 <r-kawata@digeon.co>
2025-12-01 15:31:17 +0900

    画像アップロードのひな型を作成

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,45 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
 
-  const [test, setTest] = useState("")
+  const [file, setFile] = useState<File | null>(null)
 
-  const create = async () => {
-    const response = await fetch('http://localhost:8080/ping')
+  useEffect(() => {
+    console.log(file?.name)
+  }, [file])
 
-    if (response.ok) {
-      const data = await response.json()
-      setTest(data.message)
-    } else {
-      console.error('Error creating item')
+  const change = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0] ?? null;
+
+    if (!selectedFile) {
+      setFile(null)
+      return
     }
+
+    if (selectedFile.type === "image/png" || selectedFile.type === "image/jpeg") {
+      setFile(selectedFile)
+    } else {
+      alert("PNGかJPEGの画像を選択してください")
+      setFile(null)
+    }
+  }
+
+  const upload = () => {
+    if (!file) {
+      alert("ファイルを選択してください")
+      return
+    }
+    setFile(null)
   }
 
   return (
     <>
-      <p>テスト</p>
-      <button onClick={create}>作成</button>
-      <p>{test}</p>
+      <div>
+        <input type="file" onChange={change} accept="image/png,image/jpeg"/>
+      </div>
+      <div>
+        <button onClick={upload}>アップロード</button>
+      </div>
     </>
   )
 }
